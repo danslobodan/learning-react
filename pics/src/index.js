0e6dcabaee6b4506cb8ed26b0ffda5a29fafffbd
Slobodan Dan <danastroman@gmail.com>
2020-10-18 11:44:18 +0200

    Added App component and ReactDOM.render

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,2 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <div>
+            Start
+        </div>  
+    );
+}
+
+ReactDOM.render(<App /> , document.querySelector('#root'));
