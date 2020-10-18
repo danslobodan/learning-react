75df360176ba3aca9d1ba06ef1f020008efe794a
Slobodan Dan <danastroman@gmail.com>
2020-10-18 11:52:27 +0200

    Extracted App component to file.

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,12 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const App = () => {
-    return (
-        <div>
-            Start
-        </div>  
-    );
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
 ReactDOM.render(<App /> , document.querySelector('#root'));
