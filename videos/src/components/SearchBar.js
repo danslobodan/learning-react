e804d7fa992b91ec75e99438e05cc695ae048179
Slobodan Dan <danastroman@gmail.com>
2020-10-20 21:52:56 +0200

    Added callbacks for input and form.

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -2,13 +2,29 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+    state = { term: '' };
+
+    onInputChange = e => {
+        this.setState({ term: e.target.value });
+    }
+
+    onFormSubmit = e => {
+        e.preventDefault();
+
+        // TODO: call parents callback
+    }
+
     render() {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Video Search</label>
-                        <input type="text" />
+                        <input 
+                            type="text" 
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
                     </div>
                 </form>
             </div>
@@ -16,4 +32,4 @@ class SearchBar extends React.Component {
     }
 }
 
-export default SearchBar;
+export default SearchBar;   
