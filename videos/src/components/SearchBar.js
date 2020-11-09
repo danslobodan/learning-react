d16c2c0ade9ce0da1fb0240cd1a55c5ac29a5af6
Slobodan Dan <danastroman@gmail.com>
2020-11-09 20:16:01 +0100

    Added a call to youtube API and stored results.

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
     onFormSubmit = e => {
         e.preventDefault();
 
-        // TODO: call parents callback
+        this.props.onFormSubmit(this.state.term);
     }
 
     render() {
