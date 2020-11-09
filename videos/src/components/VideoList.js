c795b80e89dfddb8a61cb0e29645ad92038f78be
Slobodan Dan <danastroman@gmail.com>
2020-11-09 20:27:24 +0100

    Mapping videos to VideoItems.

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,7 +1,14 @@
+import { render } from '@testing-library/react';
 import React from 'react';
+import VideoItem from './VideoItem';
 
 const VideoList = ({ videos }) => {
-    return <div>{videos.length}</div>
+
+    const renderedList = videos.map((video) => {
+        return <VideoItem />;
+    });
+
+    return <div>{renderedList}</div>;
 }
 
 export default VideoList;
