3ff8e469fb42ff9d4f52e58fb0360eaa2938af7b
Slobodan Dan <danastroman@gmail.com>
2020-11-10 20:16:22 +0100

    Added onTitleClick callback.

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const Accordion = ({ items }) => {
 
+    const onTitleClick = (index) => {
+        console.log('Title clicked', index);
+    }
+
     const renderedItems = items.map((item, index) => {
         return (
             <React.Fragment key={item.title}>
                 <div 
                     className="title active"
-                    onClick={() => console.log('Title clicked', index)}
+                    onClick={() => onTitleClick(index)}
                 >
                     <i className="dropdown icon"></i>
                     {item.title}
