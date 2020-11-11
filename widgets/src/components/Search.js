962002a0a46adcb97195d40c50a6787b66bd9a88
Slobodan Dan <danastroman@gmail.com>
2020-11-11 15:26:19 +0100

    added axios, useEffect hook and called Wikipedia API

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
+
+    const [term, setTerm] = useState('');
+
+    useEffect(() => {
+
+        (async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params : {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            });
+        })();
+        
+    }, [term]);
 
     return (
         <div>
             <div className="ui form">
                 <div className="field">
                     <label>Enter Search Term</label>
-                    <input className="input" />
+                    <input 
+                        className="input"
+                        value={term}
+                        onChange={(e) => setTerm(e.target.value)}
+                    />
                 </div>
             </div>
         </div>
