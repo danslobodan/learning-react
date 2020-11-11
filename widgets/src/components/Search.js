ecb961373b4e682a45400da3c683f7341cf6b2f8
Slobodan Dan <danastroman@gmail.com>
2020-11-11 16:14:07 +0100

    Removed timout for the initial search.

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -22,15 +22,20 @@ const Search = () => {
             setResults(data.query.search);
         };
 
-        const timeoutId = setTimeout(() => {
-            if (term) {
-                search();
+        if (term && !results.length) {
+            search();
+        } else {
+            const timeoutId = setTimeout(() => {
+                if (term) {
+                    search();
+                }
+            }, 500);
+            
+            return () => {
+                clearTimeout(timeoutId);
             }
-        }, 500);
-        
-        return () => {
-            clearTimeout(timeoutId)
         }
+
     }, [term]);
 
     const renderedResults = results.map((result) => {
