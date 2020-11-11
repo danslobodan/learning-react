b0d80b98e626d4b8d81d6db00b6dad8d27422506
Slobodan Dan <danastroman@gmail.com>
2020-11-11 19:09:34 +0100

    Added debounce logic for input.

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -5,15 +5,27 @@ import GOOGLETRANSLATEAPIKEY from '../secrets/googleTranslateApiKey';
 const Convert = ({ language, text }) => {
 
     const [translated, setTranslated] = useState('');
+    const [debouncedText, setDebouncedText] = useState(text);
+
+    useEffect(() => {
+
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [text]);
 
     useEffect(() => {
         
         const translate = async () => {
             const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-            params : {
-                q: text,
-                target: language.value,
-                key: GOOGLETRANSLATEAPIKEY
+                params : {
+                    q: debouncedText,
+                    target: language.value,
+                    key: GOOGLETRANSLATEAPIKEY
                 }
             });
 
@@ -24,7 +36,7 @@ const Convert = ({ language, text }) => {
             translate();
         }
 
-    }, [language, text]);
+    }, [language, debouncedText]);
 
     return (
         <div>
