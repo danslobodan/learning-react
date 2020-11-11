c488c898a8c096b8637d73fe705b1f00be577706
Slobodan Dan <danastroman@gmail.com>
2020-11-11 16:39:21 +0100

    Added options for the dropdown.

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -16,12 +16,27 @@ const items = [
         title: 'How do you use React?',
         content: 'You use React by creating components.'
     }
-]
+];
+
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Color Blue',
+        value: 'blue'
+    }
+];
 
 export default () => {
     return (
         <div>
-            <Dropdown />
+            <Dropdown optoins={options} />
         </div>
     )
 };
