efb878dc328e5fd1d7e4c45fc8a4e22252a95815
Slobodan Dan <danastroman@gmail.com>
2020-11-11 18:50:58 +0100

    Added useEffect hook when the language or text changes.

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 const Convert = ({ language, text }) => {
+
+    useEffect(() => {
+        console.log(`${text} ${language.label}`);
+    }, [language, text]);
+
     return <div></div>;
 }
 
