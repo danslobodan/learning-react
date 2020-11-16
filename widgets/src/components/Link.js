2b55cecc6ed773f2ddeca262877c743d581d33f5
Slobodan Dan <danastroman@gmail.com>
2020-11-16 20:28:16 +0100

    Replaced anchor elements with Link component.

---
+++ widgets/src/components/Link.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Link = ({ className, href, children }) => {
+
+    return (
+        <a className={className} href={href}>
+            {children}
+        </a>
+    );
+};
+
+export default Link;
