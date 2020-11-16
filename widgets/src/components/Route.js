a8ba904b2a6af4a3f52e411765de2457f8152747
Slobodan Dan <danastroman@gmail.com>
2020-11-16 20:49:41 +0100

    Forcing re-render of Route by updating state.

--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,11 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Route = ({ path, children }) => {
+
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
     useEffect(() => {
 
         const onLocationChange = () => {
-            console.log('Location Change');
+            setCurrentPath(window.location.pathname);
         }
 
         window.addEventListener('popstate', onLocationChange);
