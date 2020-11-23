a2bb35402e78ceaa6bfe846cdf836c23b499da41
Slobodan Dan <danastroman@gmail.com>
2020-11-23 20:56:55 +0100

    Displaying song information in SongDetail.

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = (props) => {
+const SongDetail = ({ song }) => {
 
-    return <div>SongDetail</div>;
+    if (!song) {
+        return <div>Select a song</div>;
+    }
+
+    return (
+        <div>
+            <h3>Details for:</h3>
+            <p>
+                Title: {song.title}
+                <br />
+                Duration: {song.duration}
+            </p>
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => {
