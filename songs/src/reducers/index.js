11cfa5b18cb491228661e9f85633f01d86456265
Slobodan Dan <danastroman@gmail.com>
2020-11-23 19:51:12 +0100

    Combined reducers.

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
 
     return [
@@ -16,3 +18,9 @@ const selectedSongReducer = (selectedSong=null, action) => {
 
     return selectedSong;
 };
+
+export default combineReducers({
+
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
