030ae274184e0f6e5dc0e24c07ec3c97ab862785
Slobodan Dan <danastroman@gmail.com>
2020-11-23 20:19:48 +0100

    Added list containers.

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -18,7 +18,7 @@ class SongList extends Component {
 
     render() {
 
-        return <div>{this.renderList()}</div>;
+        return <div className="ui divided list">{this.renderList()}</div>;
     };
 };
 
