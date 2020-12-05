d3c9daedd2beb5b86319f38dd19cc6cd5ccd1063
Slobodan Dan <danastroman@gmail.com>
2020-12-05 14:27:19 +0100

    Added react-router-dom and stream routes.

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
 
 const App = () => {
-    return <div>App</div>;
-};
+    return (
+        <div>
+            <BrowserRouter>
+                <Route path="/" exact component={StreamList} />
+                <Route path="/streams/new" exact component={StreamCreate} />
+                <Route path="/streams/show" exact component={StreamShow} />
+                <Route path="/streams/edit" exact component={StreamEdit} />
+                <Route path="/streams/delete" exact component={StreamDelete} />
+            </BrowserRouter>
+        </div>
+    )
+}
 
 export default App;
