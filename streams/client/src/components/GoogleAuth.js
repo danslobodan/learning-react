d938721ea14f2205dc2975d6c3d75ee0568edee2
Slobodan Dan <danastroman@gmail.com>
2021-01-01 19:01:32 +0100

    Initialized auth2 library.

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import CLIENTID from '../secrets/GoogleOAuthKey';
 
 class GoogleAuth extends React.Component {
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: CLIENTID,
+                scope: 'email'
+            });
+        });
+    }
 
     render() {
         return <div>Google Auth</div>;
