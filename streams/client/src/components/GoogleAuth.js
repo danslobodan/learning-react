d6f2bbe6ff572a7c0e9ea564328b5e9e56fda886
Slobodan Dan <danastroman@gmail.com>
2021-01-07 14:32:32 +0100

    Added onClick event handlers for sign in/out buttons.

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -28,19 +28,33 @@ class GoogleAuth extends React.Component {
             return null;
         } else if (this.state.isSignedIn) {
             return (
-                <button className="ui red google button">
+                <button 
+                    className="ui red google button"
+                    onClick={this.onSignOut}
+                >
                     <i className="google icon" />
                     Sign Out
                 </button>
             );
         } else {
             return (
-                <button className="ui red google button">
+                <button 
+                    className="ui red google button"
+                    onClick={this.onSignIn}
+                >
                     <i className="google icon" />
                     Sign In With Google
                 </button>
             );
         }
+    }
+
+    onSignIn = () => {
+        this.auth.signIn();
+    }
+
+    onSignOut = () => {
+        this.auth.signOut();
     }
 
     render() {
