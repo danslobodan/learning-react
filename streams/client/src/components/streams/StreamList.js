55ffa88e43990eeef88fe71f50fca57f35887b4e
Slobodan Dan <danastroman@gmail.com>
2021-01-21 23:27:53 +0100

    Fixed stream key to be stream.id

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -14,7 +14,7 @@ class StreamList extends React.Component {
 
         return this.props.streams.map(stream => {
             return (
-                <div className="item" key={stream}>
+                <div className="item" key={stream.id}>
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
