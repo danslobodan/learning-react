29e1c9bf38c40f0edaab93a548be45f25cda65aa
Slobodan Dan <danastroman@gmail.com>
2021-01-21 23:29:24 +0100

    Added edit and delete buttons and currentUserId prop.

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -6,15 +6,31 @@ import { fetchStreams } from '../../actions';
 class StreamList extends React.Component {
 
     componentDidMount() {
-        console.log('did mount')
         this.props.fetchStreams();
     }
+
+    renderAdmin = (stream) => {
+
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
+            );
+        };
+    };
 
     renderList = () => {
 
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
@@ -37,9 +53,12 @@ class StreamList extends React.Component {
     };
 };
 
-const mapStateToProps = ({ streams }) => {
-    return { streams: Object.values(streams) };
-}
+const mapStateToProps = ({ streams, auth }) => {
+    return { 
+        streams: Object.values(streams),
+        currentUserId: auth.userId
+    };
+};
 
 export default connect(
     mapStateToProps,
