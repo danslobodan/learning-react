373f270b3c9f9513856b32b2a05d7dc07e6ad4fb
Slobodan Dan <danastroman@gmail.com>
2021-02-01 17:45:30 +0100

    Added mapStateToProps and shown stream title and description.

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -9,11 +9,26 @@ class StreamShow extends React.Component {
     }
 
     render() {
-        return <div>StreamShow</div>
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+
+        const { title, description } = this.props.stream;
+
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        )
     }
 }
 
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id]};
+}
+
 export default connect(
-    null,
+    mapStateToProps,
     { fetchStream }
 )(StreamShow);
