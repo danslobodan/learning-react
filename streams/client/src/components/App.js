f77837cdb4ebb1d9b59a4f8a70d6e08a9d1be29e
Slobodan Dan <danastroman@gmail.com>
2021-02-01 17:34:58 +0100

    Added navigation to StreamShow.

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -16,7 +16,7 @@ const App = () => {
                 <Header />
                 <Route path="/" exact component={StreamList} />
                 <Route path="/streams/new" exact component={StreamCreate} />
-                <Route path="/streams/show" exact component={StreamShow} />
+                <Route path="/streams/:id" exact component={StreamShow} />
                 <Route path="/streams/edit/:id" exact component={StreamEdit} />
                 <Route path="/streams/delete/:id" exact component={StreamDelete} />
             </Router>
