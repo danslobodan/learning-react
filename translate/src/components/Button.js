3f6f3febcb8a660246714891c6a6a49428f3ce7d
Slobodan Dan <danastroman@gmail.com>
2021-02-02 21:59:38 +0100

    Refactored to use Provider in Button.

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -3,15 +3,21 @@ import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
 
-    static contextType = LanguageContext;
+    renderSubmit(language) {
+        return language === 'english' 
+            ? 'Submit' 
+            : 'Voorleggen';
+    }
 
     render() {
 
-        const text = this.context === 'english'
-            ? 'Submit'
-            : 'Voorleggen';
-
-        return <button className="ui button primary">{text}</button>
+        return (
+            <button className="ui button primary">
+                <LanguageContext.Consumer>
+                    {language => this.renderSubmit(language)}
+                </LanguageContext.Consumer>
+            </button>
+        )
     }
 }
 
