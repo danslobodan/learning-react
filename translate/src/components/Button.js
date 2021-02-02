6ecc4f873a42741926f9d9e46ed7871bee123510
Slobodan Dan <danastroman@gmail.com>
2021-02-02 22:07:25 +0100

    Added ColorContext Consumer to Button.

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
 
@@ -12,11 +13,15 @@ class Button extends React.Component {
     render() {
 
         return (
-            <button className="ui button primary">
-                <LanguageContext.Consumer>
-                    {language => this.renderSubmit(language)}
-                </LanguageContext.Consumer>
-            </button>
+            <ColorContext.Consumer>
+                {color => 
+                    <button className={`ui button ${color}`}>
+                        <LanguageContext.Consumer>
+                            {language => this.renderSubmit(language)}
+                        </LanguageContext.Consumer>
+                    </button>
+                }
+            </ColorContext.Consumer>
         )
     }
 }
