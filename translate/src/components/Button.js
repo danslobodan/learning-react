d2188a338b6328b096ac9b7ccd46ed7f9e2dc4f7
Slobodan Dan <danastroman@gmail.com>
2021-02-02 21:11:04 +0100

    Added logic to change button text depending on language.

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -6,7 +6,12 @@ class Button extends React.Component {
     static contextType = LanguageContext;
 
     render() {
-        return <button className="ui button primary">Submit</button>
+
+        const text = this.context === 'english'
+            ? 'Submit'
+            : 'Voorleggen';
+
+        return <button className="ui button primary">{text}</button>
     }
 }
 
