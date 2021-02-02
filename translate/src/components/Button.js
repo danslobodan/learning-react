90c363f78fb217380dacacff39c487c8d97da0d3
Slobodan Dan <danastroman@gmail.com>
2021-02-02 21:07:13 +0100

    Wired LanguageContext to Button.

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+
+    static contextType = LanguageContext;
+
     render() {
         return <button className="ui button primary">Submit</button>
     }
