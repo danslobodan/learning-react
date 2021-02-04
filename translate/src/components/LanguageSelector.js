be83a7e47b1c05ef05bec53dca039758a5e71ba7
Slobodan Dan <danastroman@gmail.com>
2021-02-04 15:53:45 +0100

    Extracted language selection to LanguageSelector component.

---
+++ translate/src/components/LanguageSelector.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+
+class LanguageSelector extends React.Component {
+    render() {
+        return (
+            <div>
+                Select a language
+                <i 
+                    className="flag us" 
+                    onClick={() => this.props.onLanguageChange("english")} 
+                />
+                <i 
+                    className="flag nl"
+                    onClick={() => this.props.onLanguageChange("dutch")}
+                />
+            </div>
+        )
+    }
+}
+
+export default LanguageSelector;
